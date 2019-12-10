5f99b6e7da1c8ca2c2d3c7b0c3771f7cc46a6acd
jasontmiani <jasontmiani@gmail.com>
2019-12-09 18:57:07 -0800

    yeet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PhoneInput from 'react-phone-number-input'
+import StartTrial from './Components/Form/StartTrial'
+import HiddenCaptcha from './Components/Utils/HiddenRecaptcha'
+import SelectCountryCode from "./Form"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./style.scss"
+import "style.css"
 
-export default App;
+const App = () => {
+	return (
+		<div className="App">
+			<StartTrial>
+				<HiddenCaptcha />
+			</StartTrial>
+			
+		
+		</div>
+	)
+	}
+
+  export default App
